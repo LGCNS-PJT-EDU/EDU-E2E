43ee90162de87a978d244617d0a0b95a0eb773ab
HoGyeongC <ghdy98@gmail.com>
2025-06-11 17:20:08 +0900

    test: 회원가입 후 로그인 테스트 코드 작성

---
+++ tests/conftest.py
@@ -0,0 +1,24 @@
+import pytest
+from playwright.sync_api import sync_playwright
+
+@pytest.fixture(scope="session")
+def playwright_instance():
+    with sync_playwright() as playwright:
+        yield playwright
+
+@pytest.fixture(scope="session")
+def browser(playwright_instance):
+    browser = playwright_instance.chromium.launch(headless=False, slow_mo=500)
+    yield browser
+    browser.close()
+
+@pytest.fixture
+def page(browser, request):
+    context = browser.new_context(
+        record_video_dir="videos/",  # 저장 폴더 경로
+        record_video_size={"width": 1280, "height": 720}  # 선택: 해상도
+    )
+    page = context.new_page()
+    yield page
+
+    context.close()
---
+++ tests/test_sign_up_and_login_successfully.py
@@ -0,0 +1,54 @@
+def test_example(page):
+    page.goto("http://localhost:5173")
+    assert "TakeIT" in page.title()
+
+    #회원가입으로 이동
+    page.click("text=/signup/i")
+    page.wait_for_url("**/signup")
+    assert "/signup" in page.url
+
+    # 닉네임 입력
+    page.fill("input[placeholder='닉네임']", "E2E test")
+
+    # # 잘못된 이메일 입력
+    # page.fill("input[placeholder='이메일']", "abcde")
+    # page.click("text=중복확인")
+    # page.wait_for_timeout(1000)
+    #
+    # # 중복 이메일 입력
+    # page.fill("input[placeholder='이메일']", "abc@aaa.com")
+    # page.click("text=중복확인")
+    # page.wait_for_timeout(1000)
+
+    # 사용 가능한 이메일 입력
+    page.fill("input[placeholder='이메일']", "e2etest@test.com")
+    page.click("text=중복확인")
+
+    # 비밀번호 입력
+    page.fill("input[placeholder='비밀번호']", "Test123!")
+    page.fill("input[placeholder='비밀번호 확인']", "Test123!")
+
+    # Join In 버튼 클릭
+    page.click("text=Join In")
+
+    # 회원가입 완료 후 로그인 페이지 이동 확인
+    page.wait_for_url("**/login", timeout=5000)
+    assert "/login" in page.url
+
+    #이메일 입력
+    page.fill("input#email", "e2etest@test.com")
+
+    #비밀번호 입력
+    page.fill("input#password", "Test123!")
+
+    # 로그인 버튼 클릭
+    page.click("text=CONTINUE")
+
+    try:
+        page.wait_for_url("**/roadmap", timeout=5000)
+        assert "/roadmap" in page.url
+    except:
+        page.screenshot(path="login_error.png")
+        raise AssertionError("로그인 후 dashboard로 이동하지 않음")
+
+    page.screenshot(path="sign_up_and_login_successfully.png")
